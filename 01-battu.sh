4c7322d9e822348e219f13475416cc9b172e2b6f
Nagendrareddybattu <nagendrareddybattu26@gmail.com>
2024-05-27 15:01:21 +0530

    addded new data

--- 01-battu.sh
+++ 01-battu.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-PERSON1=battu
-PERSON2=nagendra
+PERSON1=SAKETH
+PERSON2=NAGA
 
 
-echo "battu:: how are you?"
-echo "nagendra:: where are you"
-echo "battu:: you are from"
-echo "nagendra:: when you are coming"
+echo "$PERSON1:: how are you $PERSON2?"
+echo "$PERSON2:: where are you"
+echo "$PERSON1:: you are from"
+echo "$PERSON2:: when you are coming"
