752e91fbcd6ebb11d008ee8c94184e56670e43b6
Nagendrareddybattu <nagendrareddybattu26@gmail.com>
2024-05-27 15:09:13 +0530

    addded new data

---
+++ argements.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: how are you $PERSON2?"
+echo "$PERSON2:: where are you"
+echo "$PERSON1:: you are from"
+echo "$PERSON2:: when you are coming"
