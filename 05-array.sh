7942736855a4832f0c395f03483d4a657c73ae1f
Nagendrareddybattu <nagendrareddybattu26@gmail.com>
2024-05-30 06:31:14 +0530

    addded new data

---
+++ 05-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES=("JAGADEESHWARREDDY" "NAGENDRAREDDY" "SAITEJAREDDY")
+
+#SIZE OF ABOUR ARRAY =3
+#INDEX OF ARRAY = 0 1 2 
+# LIST ALWAY START WITH 0 
+
+
+echo "First movie is: ${MOVIE[O]"
+echo "First movie is: ${MOVIE[1]"
+echo "First movie is: ${MOVIE[2]"
