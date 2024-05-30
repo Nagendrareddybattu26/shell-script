51ebc571351f0164a34dd73f35a1078ca3c9546d
Nagendrareddybattu <nagendrareddybattu26@gmail.com>
2024-05-30 05:44:51 +0530

    addded new data

---
+++ 02-varibale.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+PERSON1=battu
+PERSON2=nagendra
+
+
+
+echo "battu:: how are you?"
+echo "nagendra:: where are you"
+echo "battu:: you are from"
+echo "nagendra:: you are from"
+echo "battu:: you are from"
+echo "nagendra:: you are from"
