a945d6f8f85fa3852a544b032e4bc20f7f289c8a
Nagendrareddybattu <nagendrareddybattu26@gmail.com>
2024-05-31 19:31:14 +0530

    added new data

--- 10-installation-sql.sh
+++ 10-installation-sql.sh
@@ -5,11 +5,18 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
+    exit 1 #manaully exit error
 else
     echo "You are super user."
 fi
 
 apt-get install git -y
-apt-get install mysql -y
 
+if [ $? -ne 0 ]
+then 
+    echo "installation git... FAILURE"
+    exit 1
+else
+    echo "installation of git... is SUCESS"
+fi
 
