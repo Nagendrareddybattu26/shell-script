67dbeda1279aa615671789d2e66a7017ff261f61
Nagendrareddybattu <nagendrareddybattu26@gmail.com>
2024-05-31 05:55:50 +0530

    added-new-data

--- 09-condition.sh
+++ 09-condition.sh
@@ -5,7 +5,9 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]
 
 then 
-    echo "given number $NUMBER is greater than 20 "
+    echo "given number $NUMBER is greater than 19 "
 else 
-    echo "given number $NUMBER is less than 20 "
+    echo "given number $NUMBER is less than 19 "
+if
+    echo "given number $NUMBER is equal 20"
 fi
